270788b5e7620807043d85307f70aa1de77d020d
Adam Saher <adamsaher@Adams-MacBook-Pro.local>
2021-09-20 15:06:40 -0400

    change warning msg for deleting an employee

--- public/javascripts/grid.js
+++ public/javascripts/grid.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', e => {
 
     for(let form of forms_delete) {
         form.addEventListener('submit', e => {
-            return (window.confirm('This action is irreversible. Do you want to proceed with deleting the data of the ' +
-                'selected employee?'))? true : e.preventDefault()
+            return (window.confirm('This action once executed is irreversible! Do you want to proceed with deleting the ' +
+                'data of the selected employee?'))? true : e.preventDefault()
         })
     }
 })
